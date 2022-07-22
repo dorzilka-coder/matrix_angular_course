8787c1922b749d1f06aec39680ba7f8b94030044
Dor <dor_ben1@yahoo.com>
2022-07-23 00:27:14 +0300

    strict and viewchild

--- films/src/app/subscribtion/componenets/login/login.component.ts
+++ films/src/app/subscribtion/componenets/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { FieldPasswordComponent } from '../fields-components/field-password/field-password.component';
 import {FormControl} from "@angular/forms";
 
@@ -8,14 +8,22 @@ import {FormControl} from "@angular/forms";
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, AfterViewInit {
   id: string = 'password-login'
   label: string = 'סיסמא';
   control: FormControl = new FormControl(null);
   placeholder: string = 'הזן סיסמא';
   explanationText: string = 'טקסט נוסף לקומפוננטה';
 
+
+   @ViewChild('passRef')
+    passwordRef: FieldPasswordComponent;
+
   constructor() { }
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit');
+    console.log(this.passwordRef.explanationText);
+  }
 
   ngOnInit(): void {
   }
