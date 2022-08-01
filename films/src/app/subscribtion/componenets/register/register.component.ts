e8f035a61bb1b56225326e2a4d4bf0e13fd1e193
dor.zilka <dor.zilka@icc.co.il>
2022-08-01 18:19:37 +0300

    dev

--- films/src/app/subscribtion/componenets/register/register.component.ts
+++ films/src/app/subscribtion/componenets/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { regexValidator } from '../validators/regexValidator';
+import {  verifPasswordValidator} from '../validators/verifyPasswordValidator';
+
 
 @Component({
   selector: 'app-register',
@@ -6,10 +10,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+  nameFromServer:string = 'Dor';
+  form: FormGroup;
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      "firstName": new FormControl(this.nameFromServer, Validators.required),
+       "lastName": new FormControl("", [Validators.required,regexValidator(new RegExp('[A-Za-z0-9]{5,5}'))]),
+       passwordGroup : new FormGroup({
+        "password": new FormControl("", [Validators.required,Validators.maxLength(4)]),
+        "verifyPassword": new FormControl("", [Validators.required]),
+      })
+    }) 
   }
 
+
+  pn(){
+    console.log(this.form);
+  }
+  onSubmit(){
+
+  }
+
+
+   
 }
+
+
